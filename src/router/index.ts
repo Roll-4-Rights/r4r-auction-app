09b46dd78a67ad938067c5316b9cbfe8e07fdc51
praeterusdice <your-email@example.com>
2026-08-26 16:09:15 -0400

    Update router imports for renamed kebab-case page files

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
-  { path: '/', name: 'Home', component: () => import('../pages/Home.vue') },
-  { path: '/auction', name: 'Auction', component: () => import('../pages/AuctionListings.vue') },
-  { path: '/auction/:id', name: 'ItemDetail', component: () => import('../pages/ItemDetail.vue') },
-  { path: '/account', name: 'Account', component: () => import('../pages/Account.vue') },
-  { path: '/charity', name: 'CharityInfo', component: () => import('../pages/CharityInfo.vue') },
+  { path: '/', name: 'Home', component: () => import('../pages/home-auction.vue') },
+  { path: '/auction', name: 'Auction', component: () => import('../pages/auction-listings.vue') },
+  { path: '/auction/:id', name: 'ItemDetail', component: () => import('../pages/item-detail.vue') },
+  { path: '/account', name: 'Account', component: () => import('../pages/account.vue') },
+  { path: '/charity', name: 'CharityInfo', component: () => import('../pages/charity-info.vue') },
   { path: '/current-campaign', name: 'CurrentCampaign', component: () => import('../pages/current-campaign.vue') },
-  { path: '/faq', name: 'Faq', component: () => import('../pages/Faq.vue') },
-  { path: '/donators', name: 'DonatorInfo', component: () => import('../pages/DonatorInfo.vue') },
-  { path: '/education', name: 'Education', component: () => import('../pages/EducationResources.vue') },
+  { path: '/faq', name: 'Faq', component: () => import('../pages/faq.vue') },
+  { path: '/donators', name: 'DonatorInfo', component: () => import('../pages/donator-info.vue') },
+  { path: '/education', name: 'Education', component: () => import('../pages/education-resources.vue') },
 ]
 
 export default createRouter({
