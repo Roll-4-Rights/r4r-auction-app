cfed15b41ebe482450850957dc19e08fee4a1a7e
r4rct0 <r4rcto@proton.mail>
2026-09-22 14:37:56 -0400

    adding donator info page

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://api2.roll4rights.duckdns.org/api'
+const API_BASE_URL = `${import.meta.env.VITE_API_URL}/api`
 
 export const apiService = {
   async fetchCampaignProgress() {
